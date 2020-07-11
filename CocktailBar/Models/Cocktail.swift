6e4ca54263f18347cdcfb5166773e020e0e1a0ec
a-trefilova <atrefilova97@icloud.com>
2020-07-12 02:51:38 +0500

    class for db created, insert methods doesnt work

--- CocktailBar/Models/Cocktail.swift
+++ CocktailBar/Models/Cocktail.swift
@@ -8,3 +8,21 @@
 
 import Foundation
 
+struct CurrentCocktail: Equatable {
+    let drinkId: String
+    let drinkName: String
+    let category: String
+    let isAlco: String
+    let glasses: String
+    let instructions: String
+    let imageUrl: String
+    let ingridient1: String?
+    let ingridient2: String?
+    let ingridient3: String?
+    let ingridient4: String?
+    let ingridient5: String?
+    let ingridient6: String?
+    let ingridient7: String?
+    
+
+}
