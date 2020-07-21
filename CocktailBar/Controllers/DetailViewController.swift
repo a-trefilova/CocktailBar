2ef9a3a7f6c3f1213dff543ed96f82105e8be0bf
a-trefilova <atrefilova97@icloud.com>
2020-07-21 12:18:17 +0300

    some constraints fixed

--- CocktailBar/Controllers/DetailViewController.swift
+++ CocktailBar/Controllers/DetailViewController.swift
@@ -69,6 +69,9 @@ class DetailViewController: UIViewController {
                
         instructionLabel.layer.cornerRadius = 5
         instructionLabel.clipsToBounds = true
+        
+        likeButtonOutlet.layer.cornerRadius = 5
+        likeButtonOutlet.clipsToBounds = true 
     }
     
     private func fillOutletsWithData() {
